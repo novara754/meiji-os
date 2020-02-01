b7462787a0374fa140691b47682be4c257be15e3
vzwGrey <vzwgrey@gmail.com>
2020-02-02 00:23:22 +0100

    feat: welcome message through text output

--- src/io.asm
+++ src/io.asm
@@ -29,3 +29,45 @@ move_cursor:
   mov bh, 0x00
   int 0x10
   ret
+
+; Print a single character given by AL
+; at the current position of the cursor
+printc:
+  ; 0x0E is the function to print a character
+  ; from AL
+  mov ah, 0x0E
+  ; Page 0
+  mov bh, 0x00
+  ; Black background, light grey text
+  mov bl, 0x07
+  int 0x10
+  ret
+
+; Prints a NUL-terminated string given by the
+; SI register to the screen.
+print:
+  lodsb
+  ; Check if given byte is the terminating
+  ; NUL character
+  cmp al, 0x00
+  jz .end
+  call printc
+  jmp print
+
+.end:
+  ret
+
+; Like print but will add a linebreak after
+; the given string.
+println:
+  call print
+
+  ; Move to the next line
+  mov al, `\n`
+  call printc
+
+  ; Move the cursor to the start of the line
+  mov al, `\r`
+  call printc
+
+  ret
--- src/meiji.asm
+++ src/meiji.asm
@@ -4,14 +4,22 @@
 start:
   call clear_screen
 
+  mov si, WELCOME_MSG
+  call println
+
   ; Disable interrupts and then
   ; halt until an interrupt, effectively
   ; halting the system forever.
   cli
   hlt
 
+; -- Strings --
+WELCOME_MSG: db "Welcome to the Meiji operating system.", 0
+
+; -- Includes --
 %include "io.asm"
 
+; -- Magic Number Signature --
 ; The BIOS expects to find the magic number
 ; 0xAA55 at the end of the first 512-byte sector
 ; identify the boot sector. We fill the rest of the bytes
